96727a0efb1ed0a35639eecac8f10f3e4b0460a9
Nyambevos <ruslan.bilokoniuk@gmail.com>
2024-04-14 16:35:59 +0300

    add function connect to database and model contact

---
+++ src/database/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, func
+from sqlalchemy.sql.sqltypes import DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Contact(Base):
+    __tablename__ = "contacts"
+    id = Column(Integer, primary_key=True)
+    firstname = Column(String(30), nullable=False)
+    lastname = Column(String(30), nullable=False)
+    email = Column(String(50))
+    phone = Column(String(15), nullable=False)
+    birthday = Column(DateTime)
+    created_at = Column('created_at', DateTime, default=func.now())
